90fec7fd5215456fbb5a2344bb1c8d9414daa124
lonalore <lonalore@lonalore-work-desktop>
2013-11-27 11:38:08 +0100

    some code refactoring

--- shortcodes/batch/fb_chat_shortcodes.php
+++ shortcodes/batch/fb_chat_shortcodes.php
@@ -6,6 +6,7 @@ if (!defined('e107_INIT')) {
 
 class fb_chat_shortcodes extends e_shortcode {
 
+    private $plugPrefs = array();
     private $avatar_mw = 0;
     private $avatar_mh = 0;
     private $avatar_m = null;
@@ -14,23 +15,29 @@ class fb_chat_shortcodes extends e_shortcode {
     private $avatar_p = null;
 
     function __construct() {
-        $this->avatar_mw = vartrue(e107::getPlugPref('fb_chat', 'fb_chat_menu_pic_w'), 32);
-        $this->avatar_mh = vartrue(e107::getPlugPref('fb_chat', 'fb_chat_menu_pic_h'), 32);
-        $this->avatar_pw = vartrue(e107::getPlugPref('fb_chat', 'fb_chat_page_pic_w'), 64);
-        $this->avatar_ph = vartrue(e107::getPlugPref('fb_chat', 'fb_chat_page_pic_h'), 64);
+        $this->plugPrefs = e107::getPlugConfig('fb_chat')->getPref();
+        
+        $this->avatar_mw = vartrue($this->plugPrefs['fb_chat_menu_pic_w'], 32);
+        $this->avatar_mh = vartrue($this->plugPrefs['fb_chat_menu_pic_h'], 32);
+        $this->avatar_pw = vartrue($this->plugPrefs['fb_chat_page_pic_w'], 64);
+        $this->avatar_ph = vartrue($this->plugPrefs['fb_chat_page_pic_h'], 64);
 
         $this->avatar_m = "w=" . $this->avatar_mw . "&h=" . $this->avatar_mh;
         $this->avatar_p = "w=" . $this->avatar_pw . "&h=" . $this->avatar_ph;
     }
 
-    function sc_avatar_menu() {
+    function sc_avatar_menu($text = "") {
         $tp = e107::getParser();
 
         $uid = $this->var['id'];
 
-        $genFile = e_IMAGE . "generic/blank_avatar.jpg";
-        $defAvat = e107::getPlugPref('fb_chat', 'fb_chat_nopic');
-        $avatar = vartrue($defAvat, $genFile);
+        // e107 default avatar
+        $genAvat = e_IMAGE . "generic/blank_avatar.jpg";
+        // fb_chat default avatar
+        $defAvat = $this->plugPrefs['fb_chat_nopic'];
+        // set default avatar
+        $avatar = vartrue($defAvat, $genAvat);
+        // get source url for default avatar
         $img = $tp->thumbUrl($avatar, $this->avatar_m, true);
         
         $width = $this->avatar_mw;
@@ -43,21 +50,38 @@ class fb_chat_shortcodes extends e_shortcode {
                 if (strpos($image, "://") !== false) {
                     $img = $image;
                 } elseif (substr($image, 0, 8) == "-upload-") {
-                    $image = substr($image, 8); // strip the -upload- from the beginning.
-                    if (file_exists(e_AVATAR_UPLOAD . $image)) { // Local Default Image
+                    // strip the -upload- from the beginning.
+                    $image = substr($image, 8);
+                    if (file_exists(e_AVATAR_UPLOAD . $image)) {
+                        // Local Default Image
                         $img = $tp->thumbUrl(e_AVATAR_UPLOAD . $image, $this->avatar_m);
                     }
-                } elseif (file_exists(e_AVATAR_DEFAULT . $image)) {  // User-Uplaoded Image
+                } elseif (file_exists(e_AVATAR_DEFAULT . $image)) {
+                    // User-Uplaoded Image
                     $img = $tp->thumbUrl(e_AVATAR_DEFAULT . $image, $this->avatar_m);
                 }
             }
         }
 
         $title = $this->var['name'];
-        $class = vartrue(e107::getPlugPref('fb_chat', 'fb_chat_launch'), 'fbcLaunch');        
-        $text = "<img class='img-rounded user-avatar e-tip " . $class . "' fb-data='" . $uid . "' title='" . $title . "' src='" . $img . "' alt='' style='width:" . $width . "px; height:" . $height . "px' />";
-
-        return $text;
+        $class = vartrue($this->plugPrefs['fb_chat_launch'], 'fbcLaunch');
+        
+        $attrs = array(
+            'class' => 'user-avatar e-tip ' . $class,
+            'src' => $img,
+            'fb-data' => $uid,
+            'width' => $width,
+            'height' => $height,
+            'title' => $title,
+            'alt' => $title,
+            'style' => 'width:' . $width . 'px;height:' . $height . 'px;',
+        );
+        
+        foreach ($attrs as $key => $value) {
+            $text .= $key . "='" . $value . "' ";
+        }
+        
+        return "<img " . $text . "/>";
     }
 
 }
