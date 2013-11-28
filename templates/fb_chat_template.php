aed7f065f27a27b0c1b4021d8a88eff7d15c30ad
lonalore <lonalore@lonalore-work-desktop>
2013-11-28 10:01:54 +0100

    fix floating menu css style

--- templates/fb_chat_template.php
+++ templates/fb_chat_template.php
@@ -6,7 +6,7 @@ $FB_CHAT_TEMPLATE['MENU_END'] = '</ul>';
 $FB_CHAT_TEMPLATE['MENU_END'] .= '<div class="fbClear"></div>';
 
 $FB_CHAT_TEMPLATE['FLOAT_MENU_START'] = '<ul class="fb_chat_floating_menu">';
-$FB_CHAT_TEMPLATE['FLOAT_MENU_ITEM'] =  '<li><span class="pic">{AVATAR_FLOATING_MENU}</span><span class="name">{AVATAR_NAME}</span></li>';
+$FB_CHAT_TEMPLATE['FLOAT_MENU_ITEM'] =  '<li><div class="pic">{AVATAR_FLOATING_MENU}</div><div class="name">{AVATAR_NAME}</div></li>';
 $FB_CHAT_TEMPLATE['FLOAT_MENU_END'] = '</ul>';
 
 ?>
