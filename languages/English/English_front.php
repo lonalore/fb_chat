c562f57a034c5a2131c3aa00be1d4284966a1bc8
lonalore <lonalore@lonalore-work-desktop>
2013-12-02 15:24:30 +0100

    fix language text

--- languages/English/English_front.php
+++ languages/English/English_front.php
@@ -8,6 +8,6 @@ define('LANF_FB_CHAT_05', 'Offline');
 define('LANF_FB_CHAT_06', 'Turn off chat');
 define('LANF_FB_CHAT_07', 'Turn on chat');
 define('LANF_FB_CHAT_08', 'The chat is currently disabled.');
-define('LANF_FB_CHAT_09', 'There is no online users.');
+define('LANF_FB_CHAT_09', 'There is no online user.');
 
 ?>
