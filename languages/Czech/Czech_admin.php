c085a8835315b0166ab2932ec15c1f68133c3e75
Roland Graf <oxigen.rg@gmail.com>
2014-07-03 22:34:31 +0200

    Czech language files
    Creating Czech language files

---
+++ languages/Czech/Czech_admin.php
@@ -0,0 +1,38 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 FB_Chat - Language File.
+|
+|     $Revision v1.0 $
+|     $Date - 3.7.2014 22:24:33 $
+|     $Author - Oxigen $
++----------------------------------------------------------------------------+
+*/
+define('LANA_FB_CHAT_01', 'Nastavení');
+define('LANA_FB_CHAT_02', 'FB Chat zap./vyp.');
+define('LANA_FB_CHAT_03', 'Vzhled');
+define('LANA_FB_CHAT_04', 'Povolit Chat pro');
+define('LANA_FB_CHAT_05', 'Šíøka avataru v menu');
+define('LANA_FB_CHAT_06', 'Výška avataru v menu');
+define('LANA_FB_CHAT_07', 'Šíøka avataru na stránce');
+define('LANA_FB_CHAT_08', 'Výška avataru na stránce');
+define('LANA_FB_CHAT_09', 'Chat launch class');
+define('LANA_FB_CHAT_10', 'Chat rychlost (min)');
+define('LANA_FB_CHAT_11', 'Chat rychlost (max)');
+define('LANA_FB_CHAT_12', ' sek');
+define('LANA_FB_CHAT_13', 'Chat titulek okna');
+define('LANA_FB_CHAT_14', 'Uživatelské jméno');
+define('LANA_FB_CHAT_15', 'Zobrazit jméno nebo uživatelské jméno');
+define('LANA_FB_CHAT_16', 'Výchozí avatar');
+define('LANA_FB_CHAT_17', 'Titulek okna jako odkaz');
+define('LANA_FB_CHAT_18', 'Smajlíci');
+define('LANA_FB_CHAT_19', 'Základní nastavení');
+define('LANA_FB_CHAT_20', 'Chat avatar');
+define('LANA_FB_CHAT_21', 'Vzhled');
+define('LANA_FB_CHAT_22', 'Styl zobrazení datumu');
+define('LANA_FB_CHAT_23', 'Vytváøet klikací odkazy');
+define('LANA_FB_CHAT_24', 'Vytvoøit pøipojitelná Audio/Video HTML');
+define('LANA_FB_CHAT_25', 'Plovoucí on-line seznam');
+define('LANA_FB_CHAT_26', 'Chat rychlost (menu)');
+
+?>
---
+++ languages/Czech/Czech_front.php
@@ -0,0 +1,21 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 FB_Chat - Language File.
+|
+|     $Revision v1.0 $
+|     $Date - 3.7.2014 22:24:33 $
+|     $Author - Oxigen $
++----------------------------------------------------------------------------+
+*/
+define('LANF_FB_CHAT_01', 'Chat s uživateli');
+define('LANF_FB_CHAT_02', 'Seznam uživatelù...');
+define('LANF_FB_CHAT_03', 'Poslední zpráva:');
+define('LANF_FB_CHAT_04', 'Online');
+define('LANF_FB_CHAT_05', 'Offline');
+define('LANF_FB_CHAT_06', 'Vypnout chat');
+define('LANF_FB_CHAT_07', 'Zapnout chat');
+define('LANF_FB_CHAT_08', 'Chat je momentálnì vypnutý.');
+define('LANF_FB_CHAT_09', 'Žádný uživatel není momentálnì dostupný.');
+
+?>
